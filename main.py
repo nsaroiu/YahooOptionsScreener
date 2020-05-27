a7e3f829b68aa77c5cceaa3db086b14c9e8beb4f
Stefan Saroiu <ssaroiu@hotmail.com>
2020-05-26 20:37:14 -0700

    simple example of using yfinance

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+import yfinance as yf
+
+# Get the Facebook ticker
+fb = yf.Ticker("FB")
+
+# Print the options' expiration dates
+print fb.options
+
+# Nicholas: 
+# Create an list that contains only the expiration dates that are at least 14 months into the future
+# Add your code below
