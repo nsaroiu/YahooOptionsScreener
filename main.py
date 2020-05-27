0a7f816af4534777052237ea7bfa2ef49892d103
Nicholas Saroiu <nsaroiu@hotmail.com>
2020-05-26 23:56:34 -0700

    14 Months Expiration Dates
    Calculates all availavle expiration dates for Facebook options that are at least 14 months in the future.

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import yfinance as yf
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 # Get the Facebook ticker
 fb = yf.Ticker("FB")
 
-# Print the options' expiration dates
-print fb.options
+# Gets the date 14 months from now and typecasts it to a string
+date = date.today() + relativedelta(months=+14)
+date = str(date)
 
-# Nicholas: 
-# Create an list that contains only the expiration dates that are at least 14 months into the future
-# Add your code below
+# Typecasts the expiration dates of all Facebook options to a list to make it easier to handle 
+options = list(fb.options)
+
+# Creates empty list that will contain all dates past 14 months
+eligible_options = []
+
+# Compares each option expiration date to the date 14 months from now
+for exp_date in range(len(options)):
+	if (options[exp_date] >= date):
+		eligible_options.append(options[exp_date])
+
+# Prints the list of expiration dates past 14 months
+print(eligible_options)
