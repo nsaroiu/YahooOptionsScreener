607b5c7eb64a226df4e48ff2feb8542dbf783743
Nicholas Saroiu <nsaroiu@hotmail.com>
2020-05-28 08:59:01 -0700

    Printing Call Options

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 from datetime import date
 from dateutil.relativedelta import relativedelta
+import pandas as pd
 
 # Get the Facebook ticker
 fb = yf.Ticker("FB")
@@ -16,9 +17,17 @@ options = list(fb.options)
 eligible_options = []
 
 # Compares each option expiration date to the date 14 months from now
-for exp_date in range(len(options)):
-	if (options[exp_date] >= date):
-		eligible_options.append(options[exp_date])
+for exp_date in options:
+	if (exp_date >= date):
+		eligible_options.append(exp_date)
 
-# Prints the list of expiration dates past 14 months
-print(eligible_options)
+# When printing a DataFrame, this makes sure it incudes all the details, and not just the abridged details
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+# Takes all the call options of each eligible expiration date and prints them
+for exp_date in eligible_options:
+	table = fb.option_chain(eligible_options[0]).calls
+	table = str(table)
+	print(exp_date + "\n\n")
+	print(table + "\n\n---------------------------------------------------------\n")
+
