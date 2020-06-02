08b2ae84df2a2239d97465f04fe3d8a1d4c3074e
Nicholas Saroiu <nsaroiu@hotmail.com>
2020-06-02 13:46:36 -0700

    Display Custom Columns
    Still need to work on getting the specific strike price.

--- main.py
+++ main.py
@@ -11,18 +11,19 @@ date = str(date)
 # Typecasts the expiration dates of all Facebook options to a list to make it easier to handle 
 options = list(fb.options)
 
-# Creates empty list that will contain all dates past 14 months
+# Creates empty list that will contain all dates past 12 months
 eligible_options = []
 
-# Compares each option expiration date to the date 14 months from now
+# Compares each option expiration date to the date 12 months from now
 for exp_date in options:
 	if (exp_date >= date):
 		eligible_options.append(exp_date)
 
 # Takes all the call options of each eligible expiration date and prints them
 for exp_date in eligible_options:
-	table_as_dataframe = fb.option_chain(eligible_options[0]).calls
-	table_as_string = table_as_dataframe.to_string()
-	print(exp_date + "\n\n")
-	print(table_as_string + "\n\n---------------------------------------------------------\n")
+	table_as_dataframe = fb.option_chain(exp_date).calls
+	table_as_string = table_as_dataframe.to_string(columns=['contractSymbol', 'lastTradeDate', 'strike', 'lastPrice', 'bid', 'ask', 'change', 'volume', 'openInterest'])
+	print("---------------------------------------------------------\n\n" + exp_date + "\n\n")
+	print(table_as_string + "\n\n")
+
 
