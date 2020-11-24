0ae6ac4e6c9ba5d35d9b51c4c4f1a78b725c55ea
Stefan Saroiu <ssaroiu@microsoft.com>
2020-11-23 18:00:35 -0800

    cleaned up var names

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ parser = argparse.ArgumentParser(description=description)
 parser.add_argument('ticker', help='Stock ticker (not option ticker)')
 args = parser.parse_args()
 
-# Get the Facebook ticker and share price
-fb = yf.Ticker(args.ticker)
-fbInfo = fb.info
-share_price = fbInfo["ask"]
+# Get the ticker and share price
+ticker = yf.Ticker(args.ticker)
+tickerInfo = ticker.info
+share_price = tickerInfo["ask"]
 # If the market is closed, then the share price would be equal to the previous closing price
 if (share_price == 0):
-	share_price = fbInfo["previousClose"]
+	share_price = tickerInfo["previousClose"]
 
 # Get the minimum and maximum percentage of the share price
 # as a boundary for the strike price
@@ -28,7 +28,7 @@ date = date.today() + timedelta(weeks = 12 * 4)
 date = str(date)
 
 # Typecasts the expiration dates of all Facebook options to a list to make it easier to handle 
-options = list(fb.options)
+options = list(ticker.options)
 
 # Creates empty list that will contain all dates past 12 months
 eligible_options = []
@@ -41,7 +41,7 @@ for exp_date in options:
 # Takes all the call options of each eligible expiration date, filters 
 # the strike price into the range of the minimum and maximum determined earlier, and prints them
 for exp_date in eligible_options:
-	table_as_dataframe = fb.option_chain(exp_date).calls
+	table_as_dataframe = ticker.option_chain(exp_date).calls
 	table_as_dataframe = table_as_dataframe.loc[(table_as_dataframe['strike'] >= strike_min) & (table_as_dataframe['strike'] <= strike_max)]
 	table_as_string = table_as_dataframe.to_string(columns=['contractSymbol', 'lastTradeDate', 'strike', 'lastPrice', 'bid', 'ask', 'change', 'volume', 'openInterest'])
 	print("---------------------------------------------------------\n\n" + exp_date + "\n\n")
