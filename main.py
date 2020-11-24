4e09da03e0d414201f06e4ba3cc44f10e398c881
Stefan Saroiu <ssaroiu@microsoft.com>
2020-11-23 19:58:24 -0800

    added strike parameter

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import yfinance as yf
 description = 'Options Screener using Yahoo Data API'
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument('ticker', help='Stock ticker (not option ticker)')
+screener = parser.add_argument_group('screener')
+screener.add_argument('--strike', nargs=2, metavar=('min', 'max'), type=int, default = [0.5, 0.7], help="Filter by strike btw. min and max")
 args = parser.parse_args()
 
 # Get the ticker and share price
@@ -20,8 +22,8 @@ if (share_price == 0):
 
 # Get the minimum and maximum percentage of the share price
 # as a boundary for the strike price
-strike_min = share_price * .5
-strike_max = share_price * .7
+strike_min = args.strike[0]
+strike_max = args.strike[1]
 
 # Gets the date 12 months from now and typecasts it to a string
 date = date.today() + timedelta(weeks = 12 * 4)
